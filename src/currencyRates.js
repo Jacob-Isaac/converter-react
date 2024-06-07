7159962607e9b4b30394b2abf69edd5202e0bf0b
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2024-06-07 23:51:03 +0200

    source of currency rates changed (previous was broken)

--- src/currencyRates.js
+++ src/currencyRates.js
@@ -5,15 +5,25 @@ const useCurrencyRates = () => {
   const multiplierEuro = JSON.parse(localStorage.getItem("eur"));
   const [messageFromApi, setMessageFromApi] = React.useState("loading");
   const [dateFromApi, setDateFromApi] = React.useState();
+  const myHeaders = new Headers();
+  myHeaders.append("apikey", "Zpg3QG6OAFIebXRefpbHqCP0tZJFb42M");
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+    headers: myHeaders,
+  };
 
   React.useEffect(() => {
-   (async () => {
-    try {
-    const response = await fetch("https://api.exchangerate.host/latest?base=PLN&symbols=EUR,USD,PLN");
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    const currencies = await response.json();  
+    (async () => {
+      try {
+        const response = await fetch(
+          "https://api.apilayer.com/exchangerates_data/latest?symbols=EUR%2CUSD%2CPLN&base=PLN",
+          requestOptions
+        );
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const currencies = await response.json();
         const timeout = setTimeout(() => {
           localStorage.setItem("usd", JSON.stringify(currencies.rates.USD));
           localStorage.setItem("eur", JSON.stringify(currencies.rates.EUR));
@@ -23,23 +33,23 @@ const useCurrencyRates = () => {
         return () => clearTimeout(timeout);
       } catch (error) {
         if (multiplierDollar && multiplierEuro) {
-             const timeout = setTimeout(() => {
-                setMessageFromApi("almostError");
-              }, 2000);
-             const thenTimeout = setTimeout(() => {
-                console.error("SERVER UNREACHABLE, I'M USING LOCAL DATA", error);
-                setMessageFromApi("almostOk");
-              }, 8000);
-             return () => clearTimeout(timeout, thenTimeout);
-            } else {
-              const timeout = setTimeout(() => {
-                console.error("SERVER UNREACHABLE", error);
-                setMessageFromApi("error");
-              }, 2000);
-              return () => clearTimeout(timeout);
+          const timeout = setTimeout(() => {
+            setMessageFromApi("almostError");
+          }, 2000);
+          const thenTimeout = setTimeout(() => {
+            console.error("SERVER UNREACHABLE, I'M USING LOCAL DATA", error);
+            setMessageFromApi("almostOk");
+          }, 8000);
+          return () => clearTimeout(timeout, thenTimeout);
+        } else {
+          const timeout = setTimeout(() => {
+            console.error("SERVER UNREACHABLE", error);
+            setMessageFromApi("error");
+          }, 2000);
+          return () => clearTimeout(timeout);
+        }
       }
-    }
-      })()
+    })();
   }, []);
 
   return { multiplierDollar, multiplierEuro, messageFromApi, dateFromApi };
